457f8ed15aea88bfd33be7510d36a60527068146
Nicky T <nicky.tching@grenoble-inp.org>
2022-08-15 15:17:24 +0200

    remove folder

--- src/hex/usecases/retrieve-all-pizza/retrieveAllPizzas.ts
+++
@@ -1,8 +0,0 @@
-import { AppThunk } from "../../../store/initReduxStore";
-import { retrieveAPizzaAction } from "../action";
-
-export const retrieveAPizza = (id: string): AppThunk => async (dispatch, getState, { pizzaGateway }) => {
-    const pizza = await pizzaGateway.byId(id)
-    dispatch(retrieveAPizzaAction({ pizza: pizza }))
-
-}
